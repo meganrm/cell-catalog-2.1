9ce909217d6a16061fd602126a709eaae5480735
meganrm <meganrm@gmail.com>
2024-02-05 21:34:54 -0800

    remove product

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,6 @@ import uploadcare from 'decap-cms-media-library-uploadcare'
 import cloudinary from 'decap-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 import CellLinePreview from './preview-templates/CellLinePreview'
 import GeneNamePreview from "./preview-templates/GeneNamePreview";
@@ -13,6 +12,5 @@ CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('cell-line', CellLinePreview)
 CMS.registerPreviewTemplate("gene-name", GeneNamePreview);
