75e34e8d4641f6aa536b2d3ff22435111a4a4624
meganrm <meganrm@gmail.com>
2024-02-05 22:15:29 -0800

    update index

--- src/components/CellLineTable.js
+++ src/components/CellLineTable.js
@@ -1,73 +1,82 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql, StaticQuery } from 'gatsby'
-
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql, StaticQuery } from "gatsby";
 
 const CellLineTableTemplate = (props) => {
-  
-  const { edges: cellLines } = props.data.allMarkdownRemark;
+    const { edges: cellLines } = props.data.allMarkdownRemark;
 
-  return (
-      <table className="">
-          {cellLines &&
-              cellLines.map(({ node: cellLine }) => (
-                  <tr className="" key={cellLine.id}>
-                      <td className="is-child">
-                          <Link to={cellLine.fields.slug}>
-                              <h4>AICS-{cellLine.frontmatter.cell_line_id}</h4>
-                          </Link>
-                      </td>
-                      <td>
-                          <p>{cellLine.frontmatter.clone_number}</p>
-                      </td>
-                      <td>
-                          <p>{cellLine.frontmatter.terminal_tagged}</p>
-                      </td>
-                  </tr>
-              ))}
-      </table>
-  );
-}
+    return (
+        <table className="">
+            <thead>
+                <tr>
+                    <th>Cell Line ID</th>
+                    <th>Clone Number</th>
+                    <th>Terminal Tag</th>
+                </tr>
+            </thead>
+            <tbody>
+                {cellLines &&
+                    cellLines.map(({ node: cellLine }) => (
+                        <tr className="" key={cellLine.id}>
+                            <td className="is-child">
+                                <Link to={cellLine.fields.slug}>
+                                    <h4>
+                                        AICS-{cellLine.frontmatter.cell_line_id}
+                                    </h4>
+                                </Link>
+                            </td>
+                            <td>
+                                <p>{cellLine.frontmatter.clone_number}</p>
+                            </td>
+                            <td>
+                                <p>{cellLine.frontmatter.terminal_tagged}</p>
+                            </td>
+                        </tr>
+                    ))}
+            </tbody>
+        </table>
+    );
+};
 
 CellLineTable.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
+    data: PropTypes.shape({
+        allMarkdownRemark: PropTypes.shape({
+            edges: PropTypes.array,
+        }),
     }),
-  }),
-}
+};
 
 export default function CellLineTable() {
-  return (
-      <StaticQuery
-          query={graphql`
-              query CellLineTableQuery {
-                  allMarkdownRemark(
-                      filter: {
-                          frontmatter: { templateKey: { eq: "cell-line" } }
-                      }
-                  ) {
-                      edges {
-                          node {
-                              excerpt(pruneLength: 400)
-                              id
-                              fields {
-                                  slug
-                              }
-                              frontmatter {
-                                  templateKey
-                                  cell_line_id
-                                  clone_number
-                                  terminal_tagged
-                              }
-                          }
-                      }
-                  }
-              }
-          `}
-          render={(data, count) => (
-              <CellLineTableTemplate data={data} count={count} />
-          )}
-      />
-  );
+    return (
+        <StaticQuery
+            query={graphql`
+                query CellLineTableQuery {
+                    allMarkdownRemark(
+                        filter: {
+                            frontmatter: { templateKey: { eq: "cell-line" } }
+                        }
+                    ) {
+                        edges {
+                            node {
+                                excerpt(pruneLength: 400)
+                                id
+                                fields {
+                                    slug
+                                }
+                                frontmatter {
+                                    templateKey
+                                    cell_line_id
+                                    clone_number
+                                    terminal_tagged
+                                }
+                            }
+                        }
+                    }
+                }
+            `}
+            render={(data, count) => (
+                <CellLineTableTemplate data={data} count={count} />
+            )}
+        />
+    );
 }
--- src/pages/index.md
+++ src/pages/index.md
@@ -1,70 +1,25 @@
 ---
 templateKey: index-page
-title: Great coffee with a conscience
+title: Cell Catalog
+subtitle: The catalog below provides a listing of our completed disease cell lines
 image: /img/home-jumbotron.jpg
-heading: Great coffee with a conscience
-subheading: Support sustainable farming while enjoying a cup
-mainpitch:
-  title: Why Kaldi
-  description: >
-    Kaldi is the coffee store for everyone who believes that great coffee
-    shouldn't just taste good, it should do good too. We source all of our beans
-    directly from small scale sustainable farmers and make sure part of the
-    profits are reinvested in their communities.
-description: >-
-  Kaldi is the ultimate spot for coffee lovers who want to learn about their
-  java’s origin and support the farmers that grew it. We take coffee production,
-  roasting and brewing seriously and we’re glad to pass that knowledge to
-  anyone.
-intro:
-  blurbs:
-    - image: /img/coffee.png
-      text: >
-        We sell green and roasted coffee beans that are sourced directly from
-        independent farmers and farm cooperatives. We’re proud to offer a
-        variety of coffee beans grown with great care for the environment and
-        local communities. Check our post or contact us directly for current
-        availability.
-    - image: /img/coffee-gear.png
-      text: >
-        We offer a small, but carefully curated selection of brewing gear and
-        tools for every taste and experience level. No matter if you roast your
-        own beans or just bought your first french press, you’ll find a gadget
-        to fall in love with in our shop.
-    - image: /img/tutorials.png
-      text: >
-        Love a great cup of coffee, but never knew how to make one? Bought a
-        fancy new Chemex but have no clue how to use it? Don't worry, we’re here
-        to help. You can schedule a custom 1-on-1 consultation with our baristas
-        to learn anything you want to know about coffee roasting and brewing.
-        Email us or call the store for details.
-    - image: /img/meeting-space.png
-      text: >
-        We believe that good coffee has the power to bring people together.
-        That’s why we decided to turn a corner of our shop into a cozy meeting
-        space where you can hang out with fellow coffee lovers and learn about
-        coffee making techniques. All of the artwork on display there is for
-        sale. The full price you pay goes to the artist.
-  heading: What we offer
-  description: >
-    Kaldi is the ultimate spot for coffee lovers who want to learn about their
-    java’s origin and support the farmers that grew it. We take coffee
-    production, roasting and brewing seriously and we’re glad to pass that
-    knowledge to anyone. This is an edit via identity...
-main:
-  heading: Great coffee with no compromises
-  description: >
-    We hold our coffee to the highest standards from the shrub to the cup.
-    That’s why we’re meticulous and transparent about each step of the coffee’s
-    journey. We personally visit each farm to make sure the conditions are
-    optimal for the plants, farmers and the local environment.
-  image1:
-    alt: A close-up of a paper filter filled with ground coffee
-    image: /img/products-grid3.jpg
-  image2:
-    alt: A green cup of a coffee on a wooden table
-    image: /img/products-grid2.jpg
-  image3:
-    alt: Coffee beans
-    image: /img/products-grid1.jpg
+main_pitch:
+  title: Allen Cell Collection
+  description: >-
+    The Allen Cell Collection (below) contains 56 high quality, certified
+    fluorescently tagged hiPSC lines that target 43 key cellular structures and
+    substructures. These are openly available to academic and commercial
+    researchers. Order our cell lines or the editing plasmids through Coriell
+    and Addgene respectively to support your research.
+
+
+    New lines are released several times a year. Subscribe to our newsletter to stay tuned and use the catalog below to see cell lines in progress.
+products:
+  - image: https://www.allencell.org/uploads/8/1/9/9/81996008/addgene-logo-1_orig.png
+    text: |
+      ​Allen Institute for Cell Science
+      ​Lab Plasmids
+  - image: https://www.allencell.org/uploads/8/1/9/9/81996008/coriell-logo-cmyk_orig.png
+    text: |
+      Allen Cell Collection on Coriell
 ---
