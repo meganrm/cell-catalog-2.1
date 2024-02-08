70b865a3669fc11a7697a078b3189f4226eb24d8
meganrm <meganrm@gmail.com>
2024-02-08 13:18:35 -0800

    remove unused link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -48,16 +48,6 @@ const Navbar = () => {
               Cell-line
             </Link>
           </li>
-          <li className="navbar-item" style={{ padding: "0px" }}>
-            <Link className="navbar-item" to="/contact">
-              Contact
-            </Link>
-          </li>
-          <li className="navbar-item" style={{ padding: "0px" }}>
-            <Link className="navbar-item" to="/contact/examples">
-              Form Examples
-            </Link>
-          </li>
           <li className="navbar-end has-text-centered">
             <a
               className="navbar-item"
