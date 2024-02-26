924849110405b1e99c7f5b579996402f54c911c9
meganrm <meganrm@gmail.com>
2024-02-26 12:28:39 -0800

    remove footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { Helmet } from "react-helmet";
-import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import "../style/bulma-style.sass";
 import "../style/custom-style.sass";
@@ -51,7 +50,6 @@ const TemplateWrapper = ({ children }) => {
       </Helmet>
       <Navbar />
       <div>{children}</div>
-      <Footer />
     </div>
   );
 };
