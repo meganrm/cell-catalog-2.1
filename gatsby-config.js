8e295817bdfd69b955114a4f225d1feae7c2acbf
meganrm <meganrm@gmail.com>
2024-03-13 17:21:30 -0700

    trying header in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,9 @@ module.exports = {
         description:
             "The Disease Collection Cell Catalog is a growing compilation of cell lines that carry mutations in genes known to cause disease. These cell lines were created by introducing a point mutation in one of the fluorescently tagged WTC-11 clonal lines from the Allen Cell Collection.",
     },
+    headers: {
+        "Content-Security-Policy" : "frame-ancestors '*.allencell.org';",
+    },
     plugins: [
         "gatsby-plugin-react-helmet",
         "gatsby-plugin-fix-fouc",
