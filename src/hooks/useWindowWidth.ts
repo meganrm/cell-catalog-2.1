e9aa7b9eaa856d7cc8ba1ce0e812886d17cd4a92
meganrm <meganrm@gmail.com>
2024-10-08 16:10:50 -0700

    initial call

--- src/hooks/useWindowWidth.ts
+++ src/hooks/useWindowWidth.ts
@@ -3,13 +3,14 @@ import { useState, useEffect } from "react";
 import { TABLET_BREAKPOINT } from "../constants";
 
 const useWindowWidth = () => {
-    const [width, SetWidth] = useState(TABLET_BREAKPOINT);
+    const [width, setWidth] = useState(TABLET_BREAKPOINT);
 
     useEffect(() => {
         const handleResize = () => {
-            SetWidth(window.innerWidth);
+            setWidth(window.innerWidth);
         };
         const debouncedHandleResize = debounce(handleResize, 200);
+        handleResize();
         window.addEventListener("resize", debouncedHandleResize);
         return () => {
             window.removeEventListener("resize", debouncedHandleResize);
