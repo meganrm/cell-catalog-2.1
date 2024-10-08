45977a563aed13d8c2387a028d0082b5ad768355
meganrm <meganrm@gmail.com>
2024-10-08 14:03:26 -0700

    external links

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,7 @@ module.exports = {
             resolve: "gatsby-transformer-remark",
             options: {
                 plugins: [
+                    { resolve: "gatsby-remark-external-links" },
                     "gatsby-remark-relative-images",
                     {
                         resolve: "gatsby-remark-images",
