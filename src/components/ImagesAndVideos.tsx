a7cc0c110bfdc3f90fb82001aea0579023e72f9a
Ruge Li <rugeli0605@gmail.com>
2024-10-11 15:53:31 -0700

    wip-image styling

--- src/components/ImagesAndVideos.tsx
+++ src/components/ImagesAndVideos.tsx
@@ -1,8 +1,18 @@
-import { Card } from 'antd';
-import { getImage, GatsbyImage } from 'gatsby-plugin-image';
-import React from 'react';
-import { GeneFrontMatter } from '../component-queries/types';
-import { formatCellLineId } from '../utils';
+import { Card } from "antd";
+import { getImage, GatsbyImage } from "gatsby-plugin-image";
+import React from "react";
+import { GeneFrontMatter } from "../component-queries/types";
+import { formatCellLineId } from "../utils";
+const {
+    titleSection,
+    container,
+    mainTitle,
+    subTitle,
+    leftTitle,
+    rightTitle,
+    imageSection,
+    caption,
+} = require("../style/images-and-videos.module.css");
 
 interface ImagesAndVideosProps {
     images: any;
@@ -11,24 +21,31 @@ interface ImagesAndVideosProps {
     videos?: any;
 }
 
-const ImagesAndVideos: React.FC<ImagesAndVideosProps> = ({ images, cellLineId }) => {
+const ImagesAndVideos: React.FC<ImagesAndVideosProps> = ({
+    images,
+    cellLineId,
+}) => {
     const mainImage = images[0];
     const imageData = getImage(mainImage.image);
     const title = (
-        <>
-            <h3>{formatCellLineId(cellLineId)}</h3>
-            <span>here is the subtitle</span>
-        </>
-    )
+        <div className={titleSection}>
+            <div className={leftTitle}>
+                <h3 className={mainTitle}>{formatCellLineId(cellLineId)}</h3>
+                <span className={subTitle}>here is the subtitle</span>
+            </div>
+            <span className={rightTitle}>Representative images for all clones</span>
+        </div>
+    );
 
     return (
         <div>
-            <Card
-                title={title}
-                style={{ width: "100%" }}
-            >
-                {imageData && <GatsbyImage image={imageData} alt='main image'></GatsbyImage>}
-                <p>{mainImage.caption}</p>
+            <Card className={container} title={title} style={{ width: "100%" }}>
+                <div className={imageSection}>
+                    {imageData && (
+                        <GatsbyImage image={imageData} alt="main image"></GatsbyImage>
+                    )}
+                </div>
+                <p className={caption}>{mainImage.caption}</p>
             </Card>
         </div>
     );
